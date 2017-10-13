76e05a37a20282e133ba194376da42e9e4b6c124
Ash Rhazaly <ashrhazaly@Ashs-MacBook-Pro.local>
2017-10-14 00:30:02 +0800

    Add style to view tag to enable smooth scrolling

--- index.ios.js
+++ index.ios.js
@@ -16,7 +16,7 @@ import AlbumList from './src/components/AlbumList';
 export default class iOStest extends Component {
   render() {
     return (
-      <View>
+      <View style={{ flex: 1}}>
         <Header headerText={'Albums'}/>
         <AlbumList />
       </View>
