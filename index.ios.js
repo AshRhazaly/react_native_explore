05c1bb84befc9c47c92c539bcbcf8835c37333a4
Ash Rhazaly <ashrhazaly@Ashs-MacBook-Pro.local>
2017-10-13 19:03:35 +0800

    Wrap components in view tag

--- index.ios.js
+++ index.ios.js
@@ -7,14 +7,19 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  View
 } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 
 export default class iOStest extends Component {
   render() {
     return (
-      <Header headerText={'Albums'}/>
+      <View>
+        <Header headerText={'Albums'}/>
+        <AlbumList />
+      </View>
     );
   }
 }
